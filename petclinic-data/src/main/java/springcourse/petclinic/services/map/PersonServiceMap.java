f70c7821a71afe831f0e50f17b7e2e20784b2e6b
timmah <email adres>
2018-07-31 15:30:08 +0200

    map services toegevoegd. close #6

---
+++ petclinic-data/src/main/java/springcourse/petclinic/services/map/PersonServiceMap.java
@@ -0,0 +1,34 @@
+package springcourse.petclinic.services.map;
+
+import springcourse.petclinic.model.Person;
+import springcourse.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PersonServiceMap extends AbstractMapService<Person, Long> implements CrudService<Person, Long> {
+
+    @Override
+    public Set<Person> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Person findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Person save(Person object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Person object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ petclinic-data/src/main/java/springcourse/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,34 @@
+package springcourse.petclinic.services.map;
+
+import springcourse.petclinic.model.PetType;
+import springcourse.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements CrudService<PetType, Long> {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
