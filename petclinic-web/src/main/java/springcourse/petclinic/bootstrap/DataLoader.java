c9a0ffe0d1cce973689e98865a05148dc3c014aa
timmah <email adres>
2018-08-23 15:19:12 +0200

    tonen van vets geimplementeerd closes #13

--- petclinic-web/src/main/java/springcourse/petclinic/bootstrap/DataLoader.java
+++ petclinic-web/src/main/java/springcourse/petclinic/bootstrap/DataLoader.java
@@ -47,7 +47,7 @@ public class DataLoader implements CommandLineRunner {
         vet1.setLastName("Axe");
 
         Vet vet2 = new Vet();
-        vet2.setId(1L);
+        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
