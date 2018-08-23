9373eb5012a8b8344775f06693701cf67c63c73e
timmah <email adres>
2018-08-23 14:33:59 +0200

    dataloader toegevoegd, closes #18

---
+++ petclinic-web/src/main/java/springcourse/petclinic/bootstrap/DataLoader.java
@@ -0,0 +1,58 @@
+package springcourse.petclinic.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import springcourse.petclinic.model.Owner;
+import springcourse.petclinic.model.Vet;
+import springcourse.petclinic.services.OwnerService;
+import springcourse.petclinic.services.VetService;
+import springcourse.petclinic.services.map.OwnerServiceMap;
+import springcourse.petclinic.services.map.VetServiceMap;
+
+// doordat de klasse een bean is wordt automatisch de run methode aangeroepen wanneer de klasse geinitialisserd wordt
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader() {
+        ownerService = new OwnerServiceMap();
+        vetService = new VetServiceMap();
+    }
+
+    // argumenten van de commandline worden in feite niet gebruikt, dit is enkel een manier om bij opstarten automstisch
+    // de code uit te voeren in de body
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Micahel");
+        owner1.setLastName("Weston");
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenane");
+
+        ownerService.save(owner1);
+        ownerService.save(owner2);
+
+        System.out.println("owners loaded..");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
+
+        Vet vet2 = new Vet();
+        vet2.setId(1L);
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
+
+        vetService.save(vet1);
+        vetService.save(vet2);
+
+        System.out.println("vets loaded..");
+    }
+}
