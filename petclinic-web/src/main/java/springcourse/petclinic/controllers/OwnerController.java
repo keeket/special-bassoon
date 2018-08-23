b9f02d9d35f2123f9b91b9c6092c1be0ee019088
timmah <email adres>
2018-08-23 15:11:52 +0200

    tonen van owners geimplementeerd closes #14

--- petclinic-web/src/main/java/springcourse/petclinic/controllers/OwnerController.java
+++ petclinic-web/src/main/java/springcourse/petclinic/controllers/OwnerController.java
@@ -1,7 +1,9 @@
 package springcourse.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springcourse.petclinic.services.OwnerService;
 
 // prepending context root met /owner
 // zie ook bij startup de RequestMappingHandlerMapping om te zien welke urls gebonden zijn
@@ -9,8 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "index", "index.html"})
-    public String ownersList() {
+    public String ownersList(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owner/index";
     }
 }
